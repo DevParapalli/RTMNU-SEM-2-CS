d63ec14a8c38731bff22230dfba36030f188b630
Devansh Parapalli <parapallidev@gmail.com>
2022-06-30 20:15:09 +0530

    Ninad bruh wot

---
+++ _others/ninad_p/string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    char str1[100],str2[100];
+    printf("Enter the string");
+    fgets(str1,sizeof(str1),stdin);
+    for(i=0;str1[i]!='\0';i++)
+    {
+        str2[i]=str1[i];
+    }
+    printf("second string str2 is %s",str2);
+    getch();
+}
+// Line 13 -> %S to %s
